5fac64daec3b6c591dc4242dfb89cd7b7267f055
Mykyta Ovsiienko <mykyta.ovsiienko@staircase.co>
2025-02-26 14:20:43 +0200

    feat(dap): add nvim-dap configuration and setup

---
+++ lua/movsiienko/plugins/dap.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python",
+            "jay-babu/mason-nvim-dap.nvim",
+            "theHamsta/nvim-dap-virtual-text",
+            "nvim-neotest/nvim-nio",
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            local dap = require "dap"
+            local ui = require "dapui"
+            require("mason").setup()
+            require("mason-nvim-dap").setup()
+
+            require("dapui").setup()
+
+            require("nvim-dap-virtual-text").setup {}
+            require("dap-python").setup("uv")
+            require("dap-python").test_runner = "pytest"
+
+            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+            vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+            -- Eval var under cursor
+            vim.keymap.set("n", "<space>?", function()
+                require("dapui").eval(nil, { enter = true })
+            end)
+
+            vim.keymap.set("n", "<F1>", dap.continue)
+            vim.keymap.set("n", "<F2>", dap.step_into)
+            vim.keymap.set("n", "<F3>", dap.step_over)
+            vim.keymap.set("n", "<F4>", dap.step_out)
+            vim.keymap.set("n", "<F5>", dap.step_back)
+            vim.keymap.set("n", "<F13>", dap.restart)
+
+
+            vim.keymap.set("n", "<leader>dn", require("dap-python").test_method)
+
+            dap.listeners.before.attach.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                ui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                ui.close()
+            end
+        end,
+    },
+}
