0ddc44a094b9a1ad7f696eda9c0fc2b64be2bb91
Mykyta Ovsiienko <mykyta.ovsiienko@staircase.co>
2025-05-23 17:46:10 +0300

    feat(oil): enable as default file explorer and map <leader>pv to Oil

--- lua/movsiienko/plugins/oil.lua
+++ lua/movsiienko/plugins/oil.lua
@@ -9,7 +9,7 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
-    require("oil").setup({ view_options = { show_hidden = true }, default_file_explorer = false })
+    require("oil").setup({ view_options = { show_hidden = true }, default_file_explorer = true })
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
