5c0aa9c0a8f9f71f3f842700fdb3d2b5a078a4c2
Mykyta Ovsiienko <mykyta.ovsiienko@gmail.com>
2025-10-01 16:23:10 +0300

    feat: add sidekick.nvim plugin config

--- lua/movsiienko/neovide.lua
+++ lua/movsiienko/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h17"
-  vim.g.neovide_refresh_rate = 144
+  vim.o.guifont = "CaskaydiaMono Nerd Font:h15"
+  vim.g.neovide_refresh_rate = 166
   vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
   vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
--- lua/movsiienko/plugins/blink.lua
+++ lua/movsiienko/plugins/blink.lua
@@ -25,7 +25,7 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = "enter" },
+    keymap = { preset = "default" },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -40,7 +40,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer"  },
+      default = { "lsp", "path", "snippets", "buffer" },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
