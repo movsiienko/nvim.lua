3ea5e61af2ddf5364cf0b8590500a03c56ccb621
Mykyta Ovsiienko <mykyta.ovsiienko@gmail.com>
2025-11-11 10:09:54 +0200

    feat(plugins): add amp and noir plugins

--- lua/movsiienko/neovide.lua
+++ lua/movsiienko/neovide.lua
@@ -1,7 +1,9 @@
 if vim.g.neovide then
   vim.o.guifont = "CaskaydiaMono Nerd Font:h15"
   vim.g.neovide_refresh_rate = 166
-  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.keymap.set("n", "<D-v>", '"+P')
+  vim.keymap.set("v", "<D-v>", '"+P')
+  vim.keymap.set("i", "<D-v>", "<C-r>+")
+  vim.keymap.set({ "n", "v" }, "<S-Insert>", '"+P')
+  vim.keymap.set("i", "<S-Insert>", "<C-r>+")
 end
