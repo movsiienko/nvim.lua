20241fb8f13a884412e3a669bbfd9fff4efa21fb
Mykyta Ovsiienko <68018445+movsiienko@users.noreply.github.com>
2026-01-12 17:53:31 +0200

    feat: migrate tree-sitter (partially)

--- lua/movsiienko/set.lua
+++ lua/movsiienko/set.lua
@@ -27,4 +27,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Enable Treesitter",
+  group = vim.api.nvim_create_augroup("enable_treesitter", {}),
+  -- Don't filter by `pattern`, install and enable Treesitter parsers for all languages.
+  callback = function()
+    -- Enable Treesitter syntax highlighting.
+    if pcall(vim.treesitter.start) then
+      -- Use Treesitter indentation and folds.
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      -- vim.wo.foldmethod = "expr"
+      -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    end
+  end,
+})
+
 vim.cmd("colorscheme rose-pine-main")
